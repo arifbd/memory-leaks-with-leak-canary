945694d46da283421f46834cc99aebe904561ad8
Md Arif <arifbd@list.ru>
2020-08-30 15:46:04 +0600

    project with memory leaks

---
+++ app/src/main/java/com/ennoblesoft/leakcanaryexample/GlobalSingleton.kt
@@ -0,0 +1,17 @@
+package com.ennoblesoft.leakcanaryexample
+
+object GlobalSingleton {
+    private val listeners = mutableListOf<GlobalSingletonListener>()
+
+    fun register(listener: GlobalSingletonListener) {
+        listeners.add(listener)
+    }
+
+    fun unregister(listener: GlobalSingletonListener) {
+        listeners.remove(listener)
+    }
+}
+
+interface GlobalSingletonListener {
+    fun onEvent()
+}
---
+++ app/src/main/java/com/ennoblesoft/leakcanaryexample/LeakActivity.kt
@@ -0,0 +1,21 @@
+package com.ennoblesoft.leakcanaryexample
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LeakActivity : AppCompatActivity() {
+    private val listener = Listener()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_leak)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        GlobalSingleton.register(listener)
+    }
+
+    private class Listener : GlobalSingletonListener {
+        override fun onEvent() {}
+    }
+}
